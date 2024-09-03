19e8d60dc2a7ce6d1b777469754154c218de63b6
Tatiana C <tativas187@gmail.com>
2024-09-02 21:58:01 -0500

    iot

---
+++ scripts_python/math.py
@@ -0,0 +1,25 @@
+n1 = 10
+n2 = 30
+
+add = n1 + n2
+
+print(add) 
+
+#Developer: Tatiana
+#Date: 26082024
+
+
+''' This is an Python example script. This
+    program let 'u add two number and show the
+    answer in your screen
+'''
+
+print("The answer is: ", add)
+
+print ("The answer is: " +str(add))
+
+print(f"The answer is:  {add}")
+
+print(f"The answer is:  {n1+n2}")
+
+print("The answer is: ",{n1+n2})
